023d03a3e3e4fd998d47ae008517439c5be4bb2b
danielcregg <daniel.cregg@gmail.com>
2018-02-06 10:01:51 +0000

    Added Book Class

---
+++ src/ie/gmit/javalabs/lab3classes/Book.java
@@ -0,0 +1,18 @@
+package ie.gmit.javalabs.lab3classes;
+
+public class Book {
+	private int noOfCopies;
+
+	public int getNoOfCopies() {
+		return noOfCopies;
+	}
+	
+	public void setNoOfCopies(int noOfCopiesThereIs) {
+	noOfCopies = noOfCopiesThereIs;
+}
+	
+//	public void setNoOfCopies(int noOfCopies) {
+//		this.noOfCopies = noOfCopies;
+//	}
+	
+}
