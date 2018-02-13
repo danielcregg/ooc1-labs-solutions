59e4133770ac5b5f4f4f21eaa015f7a554e52fe8
danielcregg <daniel.cregg@gmail.com>
2018-02-13 14:15:10 +0000

    Added Dog class

---
+++ src/ie/gmit/javalabs/labexams/DogDriver.java
@@ -0,0 +1,20 @@
+package ie.gmit.javalabs.labexams;
+
+public class DogDriver {
+
+	public static void main(String[] args) {
+
+		// Create a new Dog object (e.g. (“Rex”, 40, "Labrador")
+		Dog dog1 = new Dog("Rex", 40, "Labrador");
+
+		// Use the getter methods in combination with System.out.println() to print the Dog’s name, weight and breed.
+		System.out.println(dog1.getName() + " " + dog1.getWeight() + " " + dog1.breed);
+
+		// Use the eat method to make your dog eat 3kgs of food
+		dog1.eat(3.0f);
+
+		// Use the getter methods in combination with System.out.println() again to print the Dog’s new weight.
+		System.out.println(dog1.getName() + " " + dog1.getWeight() + " " + dog1.breed);
+	}
+
+}
