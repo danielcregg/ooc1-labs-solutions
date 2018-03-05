2dcc7bd951f22ec428e263cc5c64a89c3a1157c8
danielcregg <daniel.cregg@gmail.com>
2018-03-05 15:39:53 +0000

    Added Encapsulation Example Class

---
+++ src/ie/gmit/javalabs/encapsulation/EncapTestDriver.java
@@ -0,0 +1,11 @@
+package ie.gmit.javalabs.encapsulation;
+
+public class EncapTestDriver {
+
+	public static void main(String[] args) {
+		EncapTest encapTest = new EncapTest();
+		//encapTest.getAge();
+		System.out.println(encapTest.getAge());
+	}
+
+}
