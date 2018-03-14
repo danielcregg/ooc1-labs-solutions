3068d42d1937deb6f94173c4ebb68d8792e35e2d
danielcregg <daniel.cregg@gmail.com>
2018-03-14 16:03:51 +0000

    add animal inheritance

--- src/ie/gmit/javalabs/lab8inheritance/animal/AnimalInheritanceTest.java
+++ src/ie/gmit/javalabs/lab8inheritance/animal/AnimalInheritanceTest.java
@@ -3,8 +3,8 @@ package ie.gmit.javalabs.lab8inheritance.animal; // https://www.journaldev.com/6
 public class AnimalInheritanceTest {
 
 	public static void main(String[] args) {
-		Cat cat = new Cat(false, "Cat Food", 4, "Black");
-
+		Cat cat = new Cat(false, "Cat Food", 4);
+		
 		System.out.println("Cat is Vegetarian?: " + cat.isVegetarian());
 		System.out.println("Cat eats " + cat.getEats());
 		System.out.println("Cat has " + cat.getNoOfLegs() + " legs.");
