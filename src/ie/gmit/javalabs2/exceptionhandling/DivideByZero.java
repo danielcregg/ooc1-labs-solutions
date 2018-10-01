45691c7fe4c5e926137098d9c854c6a412537da2
danielcregg <daniel.cregg@gmail.com>
2018-10-01 16:08:26 +0100

    Amalgamated tries and catches

--- src/ie/gmit/javalabs2/exceptionhandling/DivideByZero.java
+++ src/ie/gmit/javalabs2/exceptionhandling/DivideByZero.java
@@ -3,42 +3,31 @@ package ie.gmit.javalabs2.exceptionhandling;
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
-
 public class DivideByZero {
 	public static void main(String args[]) {
 		Scanner userInput = new Scanner(System.in); // Create scanner object
 		int numerator = 0;
 		int denominator = 0;
 
+		try {
 		// Get numerator
 		System.out.print("Please enter an integer numerator: ");     
-		try {  
-			numerator = userInput.nextInt();
-		} catch (InputMismatchException inputMismatchException) {
-			System.err.printf( "\nException: %s\n", inputMismatchException );                                    
-			System.out.println("Please enter an integer.\n");
-			System.exit(-1); // Kill program
-		}
-		
+		numerator = userInput.nextInt();
 		// Get denominator
 		System.out.print("Please enter an integer denominator: ");
-		try {
-			denominator = userInput.nextInt();
-		} catch (InputMismatchException inputMismatchException) {
-			System.err.printf( "\nException: %s\n", inputMismatchException );                                    
+		denominator = userInput.nextInt();
+		// Print Result
+			System.out.println("\nResult: " + numerator/denominator);
+		} catch (InputMismatchException inputMisEx) {
+			System.err.printf( "\nException: %s\n", inputMisEx );                                    
 			System.out.println("Please enter an integer.\n");
 			System.exit(-1); // Kill program
-		}
-		
-		// Print Result
-		try {
-			System.out.println("\nResult: " + numerator/denominator);
-		} catch(ArithmeticException e) {
+		} catch(ArithmeticException ae) {
 			System.out.println("\nThere was an Arithmetic Error!");
 			System.out.println("Please check your input numbers!");
-			System.out.println( "Exception: " + e );
-			
+			System.out.println( "Exception: " + ae );
 		}
+		
 		userInput.close(); // Close Scanner object
 	} // end main
 } // end class DivideByZero
