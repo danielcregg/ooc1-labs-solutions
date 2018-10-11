5e389570b155a4130855b25a2b00afdd693a00d6
dcregg <dcregg@DCREG.gmadtree.gmit.ie>
2018-10-11 13:41:18 +0100

    Out of bounds check

---
+++ src/ie/gmit/javalabs2/exceptionhandling/OutOfBounds.java
@@ -0,0 +1,10 @@
+package ie.gmit.javalabs2.exceptionhandling;
+
+public class OutOfBounds {
+
+	public static void main(String[] args) {
+		int num[] = { 1, 2, 3, 4 };
+		System.out.println(num[5]);
+	}
+
+}
