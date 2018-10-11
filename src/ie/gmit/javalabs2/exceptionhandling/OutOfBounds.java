c37ea265a10620dfe57af21dde5bb75dd72f9fb8
dcregg <dcregg@DCREG.gmadtree.gmit.ie>
2018-10-11 13:43:08 +0100

    Add try catch

--- src/ie/gmit/javalabs2/exceptionhandling/OutOfBounds.java
+++ src/ie/gmit/javalabs2/exceptionhandling/OutOfBounds.java
@@ -4,7 +4,13 @@ public class OutOfBounds {
 
 	public static void main(String[] args) {
 		int num[] = { 1, 2, 3, 4 };
-		System.out.println(num[5]);
+
+		try {
+			System.out.println(num[5]);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Exception thrown  :" + e);
+		}
+		System.out.println("Out of the block");
 	}
 
 }
