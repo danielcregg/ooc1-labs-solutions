2123de424fcac4002794452db36c58b6dd918d64
danielcregg <daniel.cregg@gmail.com>
2018-10-13 16:05:57 +0100

    Read in byte from file and print to console

--- src/ie/gmit/javalabs2/io/FileCopyLab.java
+++ src/ie/gmit/javalabs2/io/FileCopyLab.java
@@ -1,10 +1,23 @@
 package ie.gmit.javalabs2.io;
 
+import java.io.*;
+
 public class FileCopyLab {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			// Create File input Stream to get data from file.
+			FileInputStream inputStream = new FileInputStream("in.txt");
+			// Read in byte from file and store it
+			int data = inputStream.read();
+			// Print data to console
+			System.out.println("Input data = " + data);
+		} catch (FileNotFoundException fnfe) {
+			System.out.println(fnfe);
+		} catch (IOException ioe) {
+			System.out.println(ioe);
+		}
+		
+		
 	}
-
 }
