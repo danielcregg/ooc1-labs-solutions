d445e8995683f3cd6d525b77c9b9b9ea806ffbc2
danielcregg <daniel.cregg@gmail.com>
2018-10-13 16:19:59 +0100

    Write out byte to file out.txt

--- src/ie/gmit/javalabs2/io/FileCopyLab.java
+++ src/ie/gmit/javalabs2/io/FileCopyLab.java
@@ -6,13 +6,16 @@ public class FileCopyLab {
 
 	public static void main(String[] args) {
 		FileInputStream inputStream = null;
+		FileOutputStream outputStream = null;
 		try {
 			// Create File input Stream to get data from file.
 			inputStream = new FileInputStream("in.txt");
+			// Create File output Stream to send data to file.
+			outputStream = new FileOutputStream("out.txt");
 			// Read in byte from file and store it
 			int data = inputStream.read();
-			// Print data to console "Input data = 97"
-			System.out.println("Input data = " + data);
+			// Write out byte from program and store it out.txt
+			outputStream.write(data);
 		} catch (FileNotFoundException fnfe) {
 			System.out.println(fnfe);
 			fnfe.printStackTrace();
