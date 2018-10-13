eb247bc9dac3269242f8c3ae73b2ba6231e310ae
danielcregg <daniel.cregg@gmail.com>
2018-10-13 16:16:36 +0100

    Close the inputStream when done using finally

--- src/ie/gmit/javalabs2/io/FileCopyLab.java
+++ src/ie/gmit/javalabs2/io/FileCopyLab.java
@@ -5,19 +5,29 @@ import java.io.*;
 public class FileCopyLab {
 
 	public static void main(String[] args) {
+		FileInputStream inputStream = null;
 		try {
 			// Create File input Stream to get data from file.
-			FileInputStream inputStream = new FileInputStream("in.txt");
+			inputStream = new FileInputStream("in.txt");
 			// Read in byte from file and store it
 			int data = inputStream.read();
-			// Print data to console
+			// Print data to console "Input data = 97"
 			System.out.println("Input data = " + data);
 		} catch (FileNotFoundException fnfe) {
 			System.out.println(fnfe);
+			fnfe.printStackTrace();
 		} catch (IOException ioe) {
 			System.out.println(ioe);
+			ioe.printStackTrace();
+		} finally {
+			if (inputStream != null) {
+				try {
+					inputStream.close();
+				} catch (IOException ioe) {
+					System.out.println(ioe);
+					ioe.printStackTrace();
+				}
+			}
 		}
-		
-		
 	}
 }
