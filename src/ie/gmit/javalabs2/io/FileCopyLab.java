a971de882a4f454482a8a340b456c8b44dddf907
danielcregg <daniel.cregg@gmail.com>
2018-10-13 16:28:38 +0100

    Added while loop and complete message

--- src/ie/gmit/javalabs2/io/FileCopyLab.java
+++ src/ie/gmit/javalabs2/io/FileCopyLab.java
@@ -7,15 +7,18 @@ public class FileCopyLab {
 	public static void main(String[] args) {
 		FileInputStream inputStream = null;
 		FileOutputStream outputStream = null;
+		int data = 0;
 		try {
 			// Create File input Stream to get data from file.
 			inputStream = new FileInputStream("in.txt");
 			// Create File output Stream to send data to file.
 			outputStream = new FileOutputStream("out.txt");
 			// Read in byte from file and store it
-			int data = inputStream.read();
-			// Write out byte from program and store it out.txt
-			outputStream.write(data);
+			while((data = inputStream.read()) != -1) {
+				// Write out byte from program and store it out.txt
+				outputStream.write(data);
+			}
+			System.out.println("Copy Complete!");
 		} catch (FileNotFoundException fnfe) {
 			System.out.println(fnfe);
 			fnfe.printStackTrace();
