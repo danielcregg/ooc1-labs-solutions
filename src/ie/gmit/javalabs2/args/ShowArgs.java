a4b426790f2ad126c4cc1eaf4dc24522b2aa3ad2
danielcregg <daniel.cregg@gmail.com>
2018-10-17 22:06:02 +0100

    Added new args class

---
+++ src/ie/gmit/javalabs2/args/ShowArgs.java
@@ -0,0 +1,10 @@
+package ie.gmit.javalabs2.args;
+
+public class ShowArgs {
+	public static void main(String[] args) {
+		System.out.println("The following command line arguments were passed:");
+		for (int i = 0; i < args.length; i++) {
+			System.out.println("arg[" + i + "]: " + args[i]);
+		}
+	}
+}
