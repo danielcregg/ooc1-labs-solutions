2ce90cdac7f34585a284822579d15474478ccdfb
dcregg <dcregg@DCREG.gmadtree.gmit.ie>
2018-11-15 12:36:40 +0000

    Added serialVersionUID

--- src/ie/gmit/javalabs2/serialization/Demo.java
+++ src/ie/gmit/javalabs2/serialization/Demo.java
@@ -1,9 +1,13 @@
 package ie.gmit.javalabs2.serialization;
 
 //Java code for serialization and deserialization of a Java objects state 
-import java.io.*;
+import java.io.Serializable;
 
 public class Demo implements Serializable {
+	// Used during Deserialization to verify that sender and receiver of a 
+	// serialized object have loaded classes for that object which are 
+	// compatible with respect to serialization.
+	private static final long serialVersionUID = 1L;
 	public int a;
 	public String b;
 
