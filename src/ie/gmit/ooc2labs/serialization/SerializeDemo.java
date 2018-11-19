18326b7d18224c86c1d4ff5d0043fe275d158e49
danielcregg <daniel.cregg@gmail.com>
2018-11-19 10:13:53 +0000

    Move Serialization and Deserialization into methods.

--- src/ie/gmit/ooc2labs/serialization/SerializeDemo.java
+++ src/ie/gmit/ooc2labs/serialization/SerializeDemo.java
@@ -4,29 +4,35 @@ import java.io.*; // Must import java.io to implement serialization
 
 public class SerializeDemo {
 	public static void main(String[] args) {
-		// Create Demo Object
+		// Create Demo Object you want to serialize
 		Demo demoObject1 = new Demo(1, "This is a String!");
-		// Create Output file
+		// Create Output file for the serialized object
 		String filename = "file.ser";
+		
 		// Print Object variables
 		System.out.println("a = " + demoObject1.a);
 		System.out.println("b = " + demoObject1.b);
 		// Add Space
 		System.out.println("");
+		
+		// Serialize Object
+		serializeObjectToFile(demoObject1, filename);
+		// Deserialize Object
+		serializeObjectFromFile(demoObject1, filename);
 
-		/*********************
-		 *   Serialization   *
-		 *********************/
+		System.out.println("a = " + demoObject1.a);
+		System.out.println("b = " + demoObject1.b);
+	}// Main
+	
+	private static void serializeObjectToFile(Object objectToBeSerialized, String fileOut) {
 		try {
-			// Saving of object in a file
-			
 			// Create file output stream
-			FileOutputStream fileOutStream = new FileOutputStream(filename);
+			FileOutputStream fileOutStream = new FileOutputStream(fileOut);
 			// Create Object Output Stream which writes objects to an OutputStream.
 			ObjectOutputStream out = new ObjectOutputStream(fileOutStream);
-
+	
 			// Method for serialization of object
-			out.writeObject(demoObject1);
+			out.writeObject(objectToBeSerialized);
 	
 			// Close write file
 			out.close();
@@ -38,30 +44,21 @@ public class SerializeDemo {
 		catch (IOException ioex) {
 			System.out.println("IOException caught");
 		}
-
-		////////////////////////////////////////////////////////////////////////
-		
-		// Create empty demo object to store deserialized object
-		Demo demoObject2 = null;
-
-		/*********************
-		 * Deserialization *
-		 *********************/
+	} // serializeObjectToFile
+	
+	private static void serializeObjectFromFile(Object objectToBeDeserialized, String fileToRead) {
 		try {
 			// Reading the object from a file
-			FileInputStream file = new FileInputStream(filename);
+			FileInputStream file = new FileInputStream(fileToRead);
 			ObjectInputStream in = new ObjectInputStream(file);
 
 			// Method for deserialization of object
-			demoObject2 = (Demo) in.readObject();
+			objectToBeDeserialized = in.readObject();
 
 			in.close();
 			file.close();
 
 			System.out.println("\nObject has been deserialized\n");
-
-			System.out.println("a = " + demoObject2.a);
-			System.out.println("b = " + demoObject2.b);
 		}
 		catch (InvalidClassException icex) {
 			System.out.println("InvalidClassException caught");
@@ -73,5 +70,5 @@ public class SerializeDemo {
 		catch (ClassNotFoundException cnfex) {
 			System.out.println("ClassNotFoundException caught");
 		}
-	}
-}
+	} // serializeObjectFromFile
+} // Class 
