23b9aff3c995b3ba086b61e6519dfaa14abf4544
danie <danie@10.12.4.252>
2019-02-25 11:54:13 +0000

    Added Main Class

--- src/ie/gmit/ooc1labs/lab6classes/MyClass.java
+++ src/ie/gmit/ooc1labs/lab6classes/MyClass.java
@@ -1,5 +1,5 @@
 package ie.gmit.ooc1labs.lab6classes;
 
 public class MyClass {
-
+	int x;
 }
---
+++ src/ie/gmit/ooc1labs/lab6classes/MyMainClass.java
@@ -0,0 +1,11 @@
+package ie.gmit.ooc1labs.lab6classes;
+
+public class MyMainClass {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello");
+
+	}
+
+}
