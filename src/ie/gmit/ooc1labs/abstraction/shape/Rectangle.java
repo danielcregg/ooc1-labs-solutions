ad14248274e00fee6832f76bf5bbc7db0d64d186
danielcregg <daniel.cregg@gmail.com>
2019-03-22 14:08:30 +0000

    Adding Shape for abstraction

---
+++ src/ie/gmit/ooc1labs/abstraction/shape/Rectangle.java
@@ -0,0 +1,15 @@
+package ie.gmit.ooc1labs.abstraction.shape;
+
+public class Rectangle extends Shape{
+	private int length;
+	private float width;
+	
+	@Override
+	public float calculateArea() {
+		// TODO Auto-generated method stub
+		return length * width;
+	}
+	
+	// Now do circle and triangle the create Main class and show objects.
+
+}
