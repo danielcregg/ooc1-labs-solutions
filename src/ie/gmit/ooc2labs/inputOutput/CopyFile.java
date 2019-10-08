9ad9650926ae7e29a89563b6df58c58780dd1da5
danielcregg <daniel.cregg@gmail.com>
2019-10-08 22:10:54 +0100

    Updated Try catch finally

--- src/ie/gmit/ooc2labs/inputOutput/CopyFile.java
+++ src/ie/gmit/ooc2labs/inputOutput/CopyFile.java
@@ -1,27 +1,29 @@
 package ie.gmit.ooc2labs.inputOutput;
 
 import java.io.*;
+
 public class CopyFile {
+	public static void main(String args[]) throws IOException {
+		FileInputStream in = null;
+		FileOutputStream out = null;
 
-   public static void main(String args[]) throws IOException {  
-      FileInputStream in = null;
-      FileOutputStream out = null;
+		try {
+			in = new FileInputStream("src/ie/gmit/ooc2labs/inputOutput/input.txt");
+			out = new FileOutputStream("src/ie/gmit/ooc2labs/inputOutput/output.txt");
+			int c;
 
-      try {
-         in = new FileInputStream("src/ie/gmit/javalabs2/inputOutput/input.txt");
-         out = new FileOutputStream("src/ie/gmit/javalabs2/inputOutput/output.txt");
-
-         int c;
-         while ((c = in.read()) != -1) {
-            out.write(c);
-         }
-      }finally {
-         if (in != null) {
-            in.close();
-         }
-         if (out != null) {
-            out.close();
-         }
-      }
-   }
+			while ((c = in.read()) != -1) {
+				out.write(c);
+			}
+		} catch (FileNotFoundException exceptionObject) {
+			exceptionObject.printStackTrace();
+		} finally {
+			if (in != null) {
+				in.close();
+			}
+			if (out != null) {
+				out.close();
+			}
+		}
+	}
 }
