3199bf72ea37c0d742b3cd920d963404f3e1eb54
danielcregg <daniel.cregg@gmail.com>
2019-10-08 22:57:51 +0100

    Updating IO code

--- src/ie/gmit/ooc2labs/io/CopyFile.java
+++ src/ie/gmit/ooc2labs/io/CopyFile.java
@@ -11,18 +11,19 @@ public class CopyFile {
 		try {
 			in = new FileInputStream("resources/input.txt");
 			out = new FileOutputStream("resources/output.txt");
-			int c;
+			int b; // Reading and Writing bytes but read method returns int
 
-			while ((c = in.read()) != -1) {
+			// Note: read returns an int so we are casting to a byte
+			while ((b = in.read()) != -1) {
 				// Uncomment next 7 lines to see bytes copied - http://www.asciitable.com/
-//				System.out.printf("%d - %c\n", c, c);
-//				// Add sleep
-//				try {
-//					TimeUnit.SECONDS.sleep(1);
-//				} catch (InterruptedException e) {
-//					e.printStackTrace();
-//				}
-				out.write(c);
+				System.out.printf("%d - %c\n", b, b);
+				// Add sleep
+				try {
+					TimeUnit.SECONDS.sleep(1);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				out.write(b);
 			}
 			System.out.println("Copy complete! See resources/output.txt");
 		} catch (FileNotFoundException exceptionObject) {
