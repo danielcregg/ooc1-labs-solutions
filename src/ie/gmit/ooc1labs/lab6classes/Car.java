310b10bb905029e017b2de877656aed0cce648ce
xman <xman@ML-RefVm-850168.zqqczofuwq5efk3x32gwmhjceb.ax.internal.cloudapp.net>
2022-03-01 13:52:10 +0000

    Add Car class and Main

---
+++ src/ie/gmit/ooc1labs/lab6classes/Car.java
@@ -0,0 +1,35 @@
+package ie.gmit.ooc1labs.lab6classes;
+
+public class Car {
+	
+	// Instance Variables
+	private String make;
+	private float engineSizeLitres;
+	private boolean engineRunning;
+	
+	// Getters and Setters
+	public String getMake() {
+		return make;
+	}
+	
+	public void setMake(String make) {
+		this.make = make;
+	}
+	
+	public float getEngineSizeLitres() {
+		return engineSizeLitres;
+	}
+	
+	public void setEngineSizeLitres(float engineSizeLitres) {
+		this.engineSizeLitres = engineSizeLitres;
+	}
+	
+	public boolean isEngineRunning() {
+		return engineRunning;
+	}
+	
+	public void setEngineRunning(boolean engineRunning) {
+		this.engineRunning = engineRunning;
+	}
+	
+} // End class
---
+++ src/ie/gmit/ooc1labs/lab6classes/Main.java
@@ -0,0 +1,12 @@
+package ie.gmit.ooc1labs.lab6classes;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		// Create a new Car object
+		Car carObject1 = new Car();
+		
+		carObject1.getMake();
+		
+	}
+}
