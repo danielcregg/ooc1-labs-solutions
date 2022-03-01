bfec03f62c0de399c6456f1217b244946790f3d0
xman <xman@ML-RefVm-850168.zqqczofuwq5efk3x32gwmhjceb.ax.internal.cloudapp.net>
2022-03-01 13:42:14 +0000

    Fix up

--- src/ie/gmit/ooc1labs/lab6classes/PersonRunner.java
+++ src/ie/gmit/ooc1labs/lab6classes/PersonRunner.java
@@ -7,33 +7,29 @@ public class PersonRunner {
 		// Create new person object
 		Person personObject1 = new Person();
 		
-		System.out.println(     personObject1.getWeight()     );
+		System.out.println(     personObject1.getWeightKg()     );
 		
 		Person personObject2 = new Person(81.11f);
 		
 		Person personObject3 = new Person("Daniel", 87.6f);
 		
 		System.out.println("Person 3 is " + personObject3.getName()  );
-		System.out.println("His weight is " + personObject3.getWeight()  );
+		System.out.println("His weight is " + personObject3.getWeightKg()  );
 		
 		personObject3.eat(12.5f);
 		personObject3.eat(6.3f);
 		personObject3.eat(17.7f);
 		personObject3.eat(19.999f);
 		
-		System.out.println("His new weight is " + personObject3.getWeight()  );
+		System.out.println("His new weight is " + personObject3.getWeightKg()  );
 		
 		personObject3.exercise(2);
 		personObject3.exercise(5);
 		
-		System.out.println("His new weight is " + personObject3.getWeight()  );
+		System.out.println("His new weight is " + personObject3.getWeightKg()  );
 		
 		
-		System.out.println(     personObject2.getWeight()     );
-		
-		
-		
-		
+		System.out.println(     personObject2.getWeightKg()     );
 		
 		
 //		Person personObject2 = new Person(12.765f);
