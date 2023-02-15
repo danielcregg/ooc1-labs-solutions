761486872e6c9719720057e8a30504e07e10d89f
danielcregg <daniel.cregg@gmail.com>
2023-02-15 21:09:39 +0000

    Add break demo

---
+++ src/ie/atu/labs/controlflow/loops/BreakDemo.java
@@ -0,0 +1,15 @@
+package ie.atu.labs.controlflow.loops;
+
+public class BreakDemo {
+    public static void main(String[] args) {
+        // For loop to print out the numbers 1 to 10
+        for (int i = 1; i <= 10; i++) {
+            // If i is 5, terminate then loop
+            if (i == 5) {
+                System.out.println("i = " + i + ". Breaking out of loop!");
+                break;
+            }
+            System.out.println("The value of i is " + i);
+        } // End of for loop
+    } // End of main method
+} // End of class BreakDemo
