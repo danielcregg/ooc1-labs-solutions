9348f17711ec8bbcb105d74a79708ca21d4fd1be
Daniel Cregg <daniel.cregg@atu.ie>
2023-03-15 11:30:00 +0000

    Updates

--- src/ie/atu/encapsulation/Dog.java
+++ src/ie/atu/encapsulation/Dog.java
@@ -7,33 +7,49 @@ public class Dog {
 	private String name;
 	private int ageYears;
 	
-	
-	// Default Constructor
-	public Dog() {
+	// Constructor with mui. Force user to choose a unique MUI
+	public Dog(long mui) {
+		this.mui = mui;
 	}
-	
-	// Parameterised Constructor
-	public Dog(long MUI, String name, int ageYears) {
+
+	// Constructor with mui and Name
+	public Dog(long mui, String name) {
+		this.mui = mui;
+		this.name = name;
+	}
+
+	// Parameterized Constructor
+	public Dog(long mui, String name, int ageYears) {
 		this.mui = mui;
 		this.name = name;
 		this.ageYears = ageYears;
 	}
 
 	// Getters and Setter
-	public void setName(String name) {
-		if (name == "Bingo") {
-			System.out.println("Illegal dog name");
-		} else {
-			this.name = name;
-		}
-	}
-	
 	public long getMui() {
 		return mui;
 	}
 
 	public void setMui(long mui) {
-		this.mui = mui;
+		// MUI Validation - MUI must be equal to 15 digits and not contain all zeros
+		if (String.valueOf(mui).length() != 15 || mui == 0) {
+			System.out.println("ERROR: MUI must be 15 digits and not all zeros.");
+		} else {
+			this.mui = mui;
+		}
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		// Name Validation - Name cannot be null, empty, more than 25 characters, or contain numbers
+		if (name == null || name == "" || name.length() > 25 || name.matches(".*\\d+.*")) {
+			System.out.println("ERROR: Name incorrect.");
+		} else {
+			this.name = name;
+		}
 	}
 
 	public int getAgeYears() {
@@ -41,97 +57,17 @@ public class Dog {
 	}
 
 	public void setAgeYears(int ageYears) {
-		this.ageYears = ageYears;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setAge(int ageYears) {
-		if(ageYears > 30 || ageYears < 0 ) {
-			System.out.println("This age is not possible");
+		// Age Validation - Dog age cannot be less than 0 or greater than 30
+		if (ageYears < 0 || ageYears > 30) {
+			System.out.println("ERROR: Invalid Age. Must be > 0 or < 30");
 		} else {
 			this.ageYears = ageYears;
 		}
 	}
 
-	
-	
-	
-//	// Constructor
-//	public Dog(){
-//		name = "A dog has no name";
-//		age = 1;
-//	}
-//	
-//	// Constructor
-//	public Dog(String name, int age){
-//		this.name = name;
-//		this.age = age;
-//	}
-//	
-	
-	// Constructor
-//	public Dog(){
-//		name = "Rex";
-//		age = 1;
-//	} 
-//	
-//	// Parameterized Constructor
-//	public Dog(String name, int age){
-//		this.name = name;
-//		this.age = age;
-//	} 
-//	
-	
-	
-//	// Here are my getters and setters
-//	// Getters
-//	public String getName() {
-//		return name;
-//	}
-//
-//	public int getAge() {
-//		return age;
-//	}
-//
-//	
-	
-	
-	
-	
-	
-	
-//	// Setters
-//	public void setName(String name) {
-//		if (name.length() > 25 || name == null || name == "" 
-//				|| name.matches(".*\\d+.*")) {
-//			System.out.println("ERROR: Name incorrect.");
-//		} else {
-//			this.name = name;
-//		}
-//	}
-//	
-//	public void setAge(int age) {
-//		// Age Validation
-//		if (age < 0 || age > 30) {
-//			System.out.println("ERROR: Invalid Age. Must be > 0 or < 30");
-//		} else {
-//			this.age = age;
-//		}
-//	}
-//	
-//	
-//	
-//	
-//	
-//	
-	
-	//str.matches(".*\\d+.*");
-	
-//	  @Override
-//	  public String toString() {
-//	    return "Woof!";
-//	  }
+	// toString() Method
+	@Override
+	public String toString(){
+		return "Dog [mui=" + mui + ", name=" + name + ", ageYears=" + ageYears + "]";
+	}
 }
--- src/ie/atu/encapsulation/Main.java
+++ src/ie/atu/encapsulation/Main.java
@@ -4,72 +4,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		// Creating two new dog objects
-		Dog dog1 = new Dog();
-	
-		
-		System.out.println(       dog1.getName()      );
-	
-		
-		
-		
-		// Setting dog1's name to Rex
-		dog1.setName("Rex");
-		dog1.setAge(29);
-		System.out.println(       dog1.getName()      );
-	
-		
-		// Setting dog2's name to something illegal
-	
+		Dog dogObject1 = new Dog(123456789012345L, "Bobby", 10);
 
-		
-		
-		
+		// Printing out the details of the first dog
+		System.out.println(dogObject1.toString());
 
-		
-		
-//		dog1. = "Rex";
-//		dog1.age = 2;
-//		dog2.name = "Bingo";
-//		dog2.age = 10;
-		
-//		System.out.println( "My first dogs is called : " +   dog1.name     );  
-//		System.out.println( "My first dogs age is : "  +  dog1.age    ); 
-//		System.out.println( "My second dogs is called : " +      dog2.name     );  
-//		System.out.println( "My second dogs age is : "  +  dog2.age    ); 
-//		
-//		dog2.name = "73yd3h983h37hd7389wdh7h3772dh73h27d7h327rh7ehd";
-//		dog2.age = -123213213;
-//		
-//		
-//		System.out.println( "My second dogs is called : " +      dog2.name     );  
-//		System.out.println( "My second dogs age is : "  +  dog2.age    ); 
-//		
-//		
-		
-		
-		
-//		Dog dog1 = new Dog("Bobby", 10);
-//		Dog dog2 = new Dog();
-//																		//System.out.println(dog1); // invokes myObject.toString() - composed of the name of the class, an @ symbol and the hashcode of the object in hexadecimal. 
-//		
-//		System.out.println(dog1.getName());
-//		System.out.println(dog1.getAge());
-//		System.out.println(dog2.getName());
-//		System.out.println(dog2.getAge());
-//		
-//		dog1.setName("Lassie9");
-//		dog1.setAge(31);
-//		System.out.println(dog1.getName());
-//		System.out.println(dog1.getAge());
-//		
-//		
-//		
-//		
-////		dog2.name = "122fdsfdsfdsfds";
-////		dog2.age = -13;
-////		System.out.println(dog2.name);
-////		System.out.println(dog2.age);
-//		
+		// Change MUI to 1234567890123456L
+		dogObject1.setMui(100000000000000L);
+		dogObject1.setName("BillyBobThornt");
+
+		// Printing out the details of the first dog
+		System.out.println(dogObject1.toString());
+
 	} // End Main Method
 
 } // End Class
