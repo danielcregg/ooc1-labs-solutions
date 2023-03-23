7ac6d7c90db08e426cb4fce3d00d50e09c976bff
danielcregg <daniel.cregg@gmail.com>
2023-03-23 11:03:31 +0000

    Clean up

--- src/ie/atu/polymorphism/overloading/calculator/Calculator.java
+++ src/ie/atu/polymorphism/overloading/calculator/Calculator.java
@@ -1,34 +1,39 @@
+/*
+ * This class provides a simple text-based menu for a calculator.
+ */
+
 package ie.atu.polymorphism.overloading.calculator;
 
 public class Calculator {
-	
+
+	// Simple add method
 	public double add(int a, double b) {
-		return a + b;
+		return a + b; // Implicit type casting. Returns double
 	}
-	
+
 	// Differs in number of parameters passed
 	public double add(int a, double b, int c) {
 		return a + b + c;
 	}
-	
+
 	// Differs in the type of parameters
 	public double add(double a, double b) {
 		return a + b;
 	}
-	
+
 	// Differs in the sequence of parameters
 	public double add(double a, int b) {
 		return a + b;
 	}
-	
-	// Changing the return type of method does not work!
-//	public int add(int a, double b) {
-//		return a + b;
-//	}
-	
-	// Changing the access modifer of method does not work!
-//	private double add(int a, double b) {
-//		return a + b;
-//	}
-	
+
+	// // Changing the return type of method does not work!
+	// public int add(int a, double b) {
+	// 	return a + b;
+	// }
+
+	// // Changing the access modifer of method does not work!
+	// private double add(int a, double b) {
+	// 	return a + b;
+	// }
+
 }
