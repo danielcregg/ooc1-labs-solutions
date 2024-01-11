78525fa9411777297fa7ce6ec1e9a61182279dbf
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-01-11 15:58:05 +0000

    Refactor myMethod calls in MyClass.java

--- src/ie/atu/intro/MyClass.java
+++ src/ie/atu/intro/MyClass.java
@@ -4,13 +4,12 @@ public class MyClass {
 
 	public static void main(String[] args) {
 		// System.out.println("Hi Mom");
-		
+
 		myMethod("Daniel", 100);
 		myMethod("Tom", 50);
 		myMethod("Sarah", 21);
 		myMethod("jimmy", 11);
-	
-		
+
 	}
 
 	static void myMethod(String fname, int age) {
