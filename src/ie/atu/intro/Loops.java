ba11424305a44035031d8def909f969020288608
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-01-11 15:57:25 +0000

    Refactored Loops class and added comments and method

--- src/ie/atu/intro/Loops.java
+++ src/ie/atu/intro/Loops.java
@@ -1,30 +1,42 @@
-package ie.atu.intro;
+package ie.atu.intro;   // Declare the package name
 
+// Define the class Loops
 public class Loops {
+	// Define the main method
 	public static void main(String[] args) {
+		// Declare and initialize variables a and b
 		int a = 5, b = 10;
 
+		// For loop that prints numbers from 1 to 100
 		for (int i = 1; i<=100; i++ ) {
 			System.out.print(i + " " );
 		}
 		
+		// Declare and initialize variable z
 		int z = 100;
+		// While loop that prints numbers from 100 to 1
 		while(z > 0) {
 			System.out.print(z + " ");
 			z--;
 		}
 		
+		// Declare and initialize variable myString
 		String myString = "Timmy";
+		// Print the length of myString
 		System.out.println(myString.length());
+		// Change the value of myString
 		myString = "Daniel Cregg";
+		// Print the length of myString after removing spaces
 		System.out.println(myString.replace(" ", "").length());
 		
-		// System.out.println(a*b);
+		// Call the multiply method with a and b as arguments
 		multiply(a, b);
 	}
 
+	// Define the multiply method
 	private static void multiply(int x, int y) {
+		// Print the product of x and y
 		System.out.println(x * y);
 	}
 
-}
+} // End of the Loops class
