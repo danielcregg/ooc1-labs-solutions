d0eb875f58b1a0aae3836ad2c9e1b61c340ffd97
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-02-06 11:26:07 +0000

    Refactor coin toss logic to improve readability and add comments

--- src/ie/atu/controlflow/decisionmaking/CoinToss.java
+++ src/ie/atu/controlflow/decisionmaking/CoinToss.java
@@ -9,12 +9,17 @@ public class CoinToss {
       int tossCount = 0; // Count total number of tosses
       int headsCount = 0; // Count of consecutive heads
   
-      while (headsCount < 3) {
+      while (headsCount < 3) { // While headsCount is less than 3
+        // Increment the toss count
         tossCount++;
-        boolean isHeads = random.nextBoolean();
+        // Randomly set isheads to true or false
+        boolean isHeads = random.nextBoolean(); 
+        // If isHeads is true, set result to "Heads", otherwise set it to "Tails"
         String result = isHeads ? "Heads" : "Tails";
+        // Print the result, heads or tails
         System.out.println(result);
   
+        // If heads, increment headsCount, otherwise reset it to 0
         if (isHeads) {
           headsCount++;
         } else {
