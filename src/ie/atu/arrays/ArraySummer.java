53226e67b4a68364c75c964006c223f6cf652cf5
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-02-12 15:29:39 +0000

    Refactor ArraySummer class to calculate sum and average of an array

--- src/ie/atu/arrays/ArraySummer.java
+++ src/ie/atu/arrays/ArraySummer.java
@@ -2,24 +2,34 @@ package ie.atu.arrays;
 
 public class ArraySummer {
 
-	public static void main(String[] args) {
-		
-		int[] numbers = { 1,2,3,4,5 };
+    public static void main(String[] args) {
+
+        // Constant for array size
+        final int ARRAY_SIZE = 10;
+        
+        // Variables
         int sum = 0;
-        
-        //System.out.print("Sum of ");
-        
-        for(int i=0; i < numbers.length; i++) {
-            sum = sum + numbers[i];
-//            if (i < (numbers.length - 1)) {
-//            	System.out.print(numbers[i] + " + ");
-//            } else {
-//            	System.out.print(numbers[i]);
-//            }
+        double average = 0;
+
+        // Create an array of 100 random numbers
+        int[] numbers = new int[ARRAY_SIZE];
+
+        // Populate the array with random numbers
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = (int) (Math.random() * 100);
         }
-        
-        //System.out.println(" = " + sum);
+
+        // Sum the numbers in the array
+        for (int i = 0; i < numbers.length; i++) {
+            sum = sum + numbers[i];
+        }
+
+        // Print the array using Arrays.toString
+        System.out.println(java.util.Arrays.toString(numbers)); 
+        // System.out.println(" = " + sum);
         System.out.println("Sum: " + sum);
-	}
-	
-}
+        average = (double) sum / numbers.length;
+        System.out.println("Average: " + average);
+
+    } // end of main
+} // end of class
