9309b8587e6baddfd31931688ef69fb6d07e8317
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-02-12 15:10:50 +0000

    Refactor array printing and indexing

--- src/ie/atu/arrays/ForEachLoop.java
+++ src/ie/atu/arrays/ForEachLoop.java
@@ -6,29 +6,28 @@ public class ForEachLoop {
 
 		int[] numericals = {100, 200, 300, 400, 500};
 
-		for (int i = 0; i < numericals.length; i++) {
-			System.out.print(numericals[i]);
-			System.out.print(",");
+		// for (int i = 0; i < numericals.length; i++) {
+		// 	System.out.print(numericals[i]);
+		// 	System.out.print(",");
+		// }
+
+		//System.out.print(numericals[0]);
+		
+		//System.out.println(); // adding space
+
+		for (int num : numericals) {
+			System.out.println(num);
 		}
 
 		//System.out.print(numericals[0]);
 		
-		System.out.println(); // adding space
+		//System.out.println(); // adding space
 
-		for (int u : numericals) {
-			System.out.print(u);
-			System.out.print(",");
-		}
-
-		System.out.print(numericals[0]);
-		
-		System.out.println(); // adding space
-
-		String[] titles = { "William", "Beatrice", "Lucy", "Sam" };
-		for (String name : titles) {
-			System.out.print(name);
-			System.out.print(",");
-		}
+		// String[] titles = { "William", "Beatrice", "Lucy", "Sam" };
+		// for (String name : titles) {
+		// 	System.out.print(name);
+		// 	System.out.print(",");
+		// }
 
 	}
 
