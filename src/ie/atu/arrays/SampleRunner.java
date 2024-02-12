0660f7ab8c01fed615498fedf08b5e20adb30f01
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-02-12 15:38:33 +0000

    Refactor SampleRunner.java to improve readability and remove unused code

--- src/ie/atu/arrays/SampleRunner.java
+++ src/ie/atu/arrays/SampleRunner.java
@@ -2,15 +2,16 @@ package ie.atu.arrays;
 
 public class SampleRunner {  
     public static void main(String[] args){
-        System.out.println(args + "-->" + args.length);
+        // 
+        System.out.println(args.length);
         
-        //for (int i = 0; i < args.length; i++){
-        for (int i = args.length - 1; i >= 0; i-=2){    
+        for (int i = 0; i < args.length; i++){  
             System.out.println(args[i]);
             
             if (args[i].equals("ATU")){
                 System.out.println("\tWish you were here....");
             }
         }      
-    }
-}
+
+    } // end of main
+} // end of class
