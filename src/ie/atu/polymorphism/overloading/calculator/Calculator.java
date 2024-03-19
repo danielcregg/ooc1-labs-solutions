6e7b3ba63b19725ac897da945309ccf25908a8ef
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-03-19 09:48:28 +0000

    Refactor Calculator class to demonstrate method overloading

--- src/ie/atu/polymorphism/overloading/calculator/Calculator.java
+++ src/ie/atu/polymorphism/overloading/calculator/Calculator.java
@@ -1,9 +1,6 @@
-/*
- * This class provides a simple text-based menu for a calculator.
- */
-
 package ie.atu.polymorphism.overloading.calculator;
 
+// This class demonstrates method overloading AKA compile-time polymorphism.
 public class Calculator {
 
 	// Simple add method. Note it has two input parameters, int and double
@@ -26,14 +23,16 @@ public class Calculator {
 		return a + b;
 	}
 
-//	 // Changing the return type of method does not work!
-//	 public int add(int a, double b) {
-//	 	return a + b;
-//	 }
+	// Overloading is strictly based on the number, type, and sequence of parameters
 
-//	 // Changing the access modifer of method does not work!
-//	 private double add(int a, double b) {
-//	 	return a + b;
-//	 }
+	// // Changing the return type of method does not work!
+	// public int add(int a, double b) {
+	// 	return a + b;
+	// }
 
-}
+	// // Changing the access modifer of method does not work!
+	// private double add(int a, double b) {
+	// 	return a + b;
+	// }
+
+} // End of class
