b3f65f597699cd582b96ca02797625874a92f39b
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-03-19 16:40:50 +0000

    Refactor code for better readability and formatting

--- src/ie/atu/polymorphism/overriding/human/Main.java
+++ src/ie/atu/polymorphism/overriding/human/Main.java
@@ -6,11 +6,11 @@ public class Main {
 		Human humanObj1 = new Human();
 		IrishPerson IrishPersonObj1 = new IrishPerson();
 		FrenchPerson FrenchPersonObj1 = new FrenchPerson();
-		
+
 		humanObj1.greet();
 		IrishPersonObj1.greet();
 		FrenchPersonObj1.greet();
-		
+
 		Human humanObj2 = new Human();
 		Human IrishPersonObj2 = new IrishPerson();
 		Human FrenchPersonObj2 = new FrenchPerson();
@@ -18,15 +18,17 @@ public class Main {
 		humanObj2.greet();
 		IrishPersonObj2.greet();
 		FrenchPersonObj2.greet();
-		
+
 		Human HumansList[] = { humanObj1, IrishPersonObj1, FrenchPersonObj1 };
 
-		for(Human human: HumansList) {
+		for (Human human : HumansList) {
 			human.greet();
 		}
 
-		// Here we demonstrate polymorphism. The method showPassport() is overridden in the subclasses. 
-		// Run-time polymorphism is achieved by calling the method showPassport() on the object of the subclass.
+		// Here we demonstrate polymorphism. The method showPassport() is overridden in
+		// the subclasses.
+		// Run-time polymorphism is achieved by calling the method showPassport() on the
+		// object of the subclass.
 		checkPassport(humanObj1);
 		checkPassport(IrishPersonObj1);
 		checkPassport(FrenchPersonObj1);
