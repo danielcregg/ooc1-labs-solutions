0f61bb2b67025e74597cd9edc8e36a54a27e3090
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-03-19 16:38:37 +0000

    Add bank account and cheque deposit functionality

--- src/ie/atu/polymorphism/overloading/bank/Main.java
+++ src/ie/atu/polymorphism/overloading/bank/Main.java
@@ -3,10 +3,15 @@ package ie.atu.polymorphism.overloading.bank;
 // Main class
 public class Main {
     public static void main(String[] args) {
+        
+        // Create a bank account
         BankAccount account = new BankAccount();
+        // Deposit money into the account
         account.deposit(100.0); // Deposited 100.0. New balance is 100.0
-
+        
+        // Create a cheque
         Cheque cheque1 = new Cheque(200.0);
+        // Deposit the cheque into the account
         account.deposit(cheque1); // Deposited cheque of amount 200.0. New balance is 300.0
     }
 }
