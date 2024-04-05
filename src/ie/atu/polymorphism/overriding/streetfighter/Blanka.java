1619209938c40d3bf006e0ec4c51c6a2b7a5bf2b
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-04-05 15:17:08 +0000

    Refactor Blanka class to remove unused method and update formatting

--- src/ie/atu/polymorphism/overriding/streetfighter/Blanka.java
+++ src/ie/atu/polymorphism/overriding/streetfighter/Blanka.java
@@ -6,9 +6,5 @@ public class Blanka extends Fighter {
     public void specialMove() {
         System.out.println("Electric Thunder!");
     }
-
-    // Demo dynamic method dispatch (runtime polymorphism aka method overriding)
-    public void attack(Blanka blanka) {
-        blanka.specialMove();
-    }
+    
 }
