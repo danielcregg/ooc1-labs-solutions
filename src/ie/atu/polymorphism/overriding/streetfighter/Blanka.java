fec1e51cc9b0d7ae56c398b15bd555393e493f96
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-04-08 13:53:20 +0000

    Update specialMove method in Blanka and ChunLi classes

--- src/ie/atu/polymorphism/overriding/streetfighter/Blanka.java
+++ src/ie/atu/polymorphism/overriding/streetfighter/Blanka.java
@@ -3,6 +3,7 @@ package ie.atu.polymorphism.overriding.streetfighter;
 public class Blanka extends Fighter {
 
     // Demo dynamic method dispatch (runtime polymorphism aka method overriding)
+    @Override
     public void specialMove() {
         System.out.println("Electric Thunder!");
     }
--- src/ie/atu/polymorphism/overriding/streetfighter/ChunLi.java
+++ src/ie/atu/polymorphism/overriding/streetfighter/ChunLi.java
@@ -2,6 +2,7 @@ package ie.atu.polymorphism.overriding.streetfighter;
 
 public class ChunLi extends Fighter {
 
+    @Override
     public void specialMove() {
         System.out.println("Spinning Bird Kick!");
     }
