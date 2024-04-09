d1d843db7ed29de243040b6ddb980d4c196f7795
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-04-09 10:09:51 +0000

    Update Animal interface comments for better clarity

--- src/ie/atu/abstraction/interfacedemo/Animal.java
+++ src/ie/atu/abstraction/interfacedemo/Animal.java
@@ -1,19 +1,24 @@
 package ie.atu.abstraction.interfacedemo;
 
-// An interface is a completely "abstract class" that is used 
-// to group related methods with empty bodies.
-// To access the interface, it must be "implemented"
-// Here is an example of an interface:
+/**
+ * An interface to represent an animal. This interface is used to group related
+ * methods. To access the interface, it must be "implemented".
+ */
 public interface Animal {
 
-	// Instance variables of an interface are by default public, static and final.
-	// This means that the value of the variable cannot be changed once it is 
-	// initialized, and it is shared among all instances of classes that implement
-	// the interface.
+	/**
+	 * Instance variables of an interface are by default public, static and final.
+	 * This means that the value of the variable cannot be changed once it is
+	 * initialized, and it is shared among all instances of classes that implement
+	 * the interface.
+	 */
 	public static final boolean canMove = true;
 
-	// All interface methods are by default abstract and public
-	// Interface methods do not have a body - the body is provided by the "implement" class 
+	/**
+	 * All interface methods are by default abstract and public
+	 * Interface methods do not have a body - the body is provided by the
+	 * "implement" class.
+	 */
 	public abstract void makeSound();
 
 }
